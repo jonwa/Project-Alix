2984c4313fc3cc525c55a0de8ff3260e9ea4fedd
Sebastian Olsson <S.Olsson91@gmail.com>
2014-04-03 09:38:29 +0200

    Move Componenten lagts till (funkar ej helt än)

---
+++ Assets/Scripts/Objects/Components/Move.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Discription: Move Component
+ * 
+ * Created by: Sebastian 02/04-14
+ * Modified by:
+ */
+//
+public class Move : ObjectComponent 
+{
+	public float m_distance;
+	Vector3 m_cameraPosition = Camera.current.transform.position;
+
+	void Update()
+	{
+		Interact ();
+	}
+	
+	public override void Interact()
+	{
+		RaycastHit hit;
+		Ray ray = camera.ScreenPointToRay(new Vector3(200, 200, 0));
+		//Ray m_objectRay = new Ray (m_cameraPosition, Vector3.forward);
+		//Debug.DrawRay (ray, Vector3.forward, Color.red);
+		Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+		
+		
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			if(Physics.Raycast(ray, out hit, m_distance))
+			{
+				Debug.Log("Träff" + hit);
+			}
+		}
+
+			//Moves the object forwards
+			if (Input.GetAxis("Mouse Y") > 0) 
+			{
+				
+				Debug.Log ("Rör musen upp!");
+			}
+			//Moves the object backwards
+			if (Input.GetAxis ("Mouse Y") < 0) 
+			{
+				
+				Debug.Log ("Rör musen Ner!");
+			}
+
+
+	}
+}
---
+++ Assets/Scripts/Objects/ObjectComponent.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Discription: Base class for object components
+ * 
+ * Created by: Robert and Sebastian Datum: 02/04-14
+ * Modified by:
+ * 	
+ */
+
+public class ObjectComponent : MonoBehaviour
+{
+    private bool m_isActive = false;
+   
+
+    // Use this for initialization
+    void Start()
+    {
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	public virtual void Init()
+    {
+
+    }
+
+	public virtual void Should()
+    {
+
+    }
+
+	public virtual void Interact()
+    {
+
+    }
+
+    // If raycast hits object and leftmouse is pressed object is active
+    public void Activate()
+    {
+    	m_isActive = true;
+    }
+}
