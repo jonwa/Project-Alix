7b02094516abb1fdaa740f249ee57ed1d5850511
Rasmus Björk <ramsus_89@hotmail.com>
2014-04-04 17:09:28 +0200

    PickUp

--- Assets/Scripts/Objects/Components/PickUp.cs
+++ Assets/Scripts/Objects/Components/PickUp.cs
@@ -7,7 +7,7 @@ using System.Collections;
  * 
  */
 
-[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
 public class PickUp : ObjectComponent
 {
 	#region PublicMemberVariables
@@ -22,8 +22,9 @@ public class PickUp : ObjectComponent
 	#region PrivateMemberVariables
 	private Transform   m_CameraTransform;
 	private int			m_DeActivateCounter;
+	private int			m_Collided=0;
 	private bool 		m_HoldingObject=false;
-	private bool 		m_move=true;
+	private bool 		m_Move=true;
 	#endregion
 	
 	
@@ -34,18 +35,31 @@ public class PickUp : ObjectComponent
 	
 	void Update()
 	{
-		if(!GetIsActive())
+		m_DeActivateCounter++;
+		if(m_DeActivateCounter > 10)
+		{
+			rigidbody.useGravity=true;
+			m_HoldingObject=false;
+		}
+		if(m_Collided>0)
+		{
+			m_Collided--;
+		}
+		//Interact();
+		/*if(!GetIsActive())
 		{
 			MoveToInspectDistance(false);
+			rigidbody.useGravity=true;
 			m_HoldingObject=false;
 			//m_CameraTransform.gameObject.GetComponent<FirstPersonCamera>().UnLockCamera();
 		}
 		else
 		{
+			rigidbody.useGravity=false;
 			m_DeActivateCounter++;
 			if(m_DeActivateCounter > 10)
 				DeActivate();
-		}
+		}*/
 	}
 	
 	//Lerps position and rotation of the object to the inspection Mode distance and back to original position/rotation
@@ -54,17 +68,14 @@ public class PickUp : ObjectComponent
 		Vector3 cameraPosition 		 = m_CameraTransform.position;
 		float   cameraObjectDistance = Vector3.Distance(cameraPosition, transform.position);
 		
-		if(cameraObjectDistance > m_InspectionViewDistance)
+		if(cameraObjectDistance-0.2 >= m_InspectionViewDistance)
 		{ 
 			Vector3 targetPosition;
-			if(shouldInspect)
-			{
-				Vector3 cameraForward  = m_CameraTransform.forward.normalized;
-				
-				cameraForward *= m_InspectionViewDistance;
-				targetPosition = cameraPosition+cameraForward;
-				transform.position = Vector3.Lerp(transform.position, targetPosition, m_LerpSpeed/10.0f);
-			}
+			Vector3 cameraForward  = m_CameraTransform.forward.normalized;
+			
+			cameraForward *= m_InspectionViewDistance;
+			targetPosition = cameraPosition+cameraForward;
+			transform.position = Vector3.Lerp(transform.position, targetPosition, m_LerpSpeed/10.0f);
 		}
 		else
 		{
@@ -74,49 +85,42 @@ public class PickUp : ObjectComponent
 	
 	public override void Interact ()
 	{
-		if(GetIsActive())
-		{
-			MoveToInspectDistance(true);
+		if(m_Collided==0){
+			rigidbody.useGravity=false;
+			m_DeActivateCounter=0;
 
-			if(m_HoldingObject == true && m_move == true){
+			if(m_HoldingObject == true && m_Move == true){
 				Vector3 cameraPosition = m_CameraTransform.position;
 
 				Vector3 targetPosition;
 
 				Vector3 cameraForward  = m_CameraTransform.forward.normalized;
-				
+					
 				cameraForward *= m_InspectionViewDistance;
 				targetPosition = cameraPosition+cameraForward;
 
 				transform.position = targetPosition;
 			}
-			//Vector3.Lerp(transform.position, targetPosition, m_LerpSpeed/10.0f);
-		}
-		
-		//Check if we should inspect the object or not.
-		if(Input.GetButton(m_Input))
-		{
-			Activate();
-			m_DeActivateCounter = 0;
-		}
-		else
-		{
-			DeActivate();
+			
+			//Check if we should inspect the object or not.
+			if(Input.GetButton(m_Input))
+			{
+				MoveToInspectDistance(true);
+			}
 		}
 	}
 
 	public void OnCollisionEnter()
 	{
 		Debug.Log("Krock");
-		//collider.transform.localScale.Set(1.5f, 1.5f, 1.5f);
-		//m_move=false;
+		m_Move=false;
+		m_Collided=30;
 	}
 
 	public void OnCollisionExit()
 	{
 		Debug.Log("Slut Krock");
-		//collider.transform.localScale.Set(1.0f, 1.0f, 1.0f);
-		m_move=true;
+		m_Move=true;
 	}
 
 }
