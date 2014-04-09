268c1d5de53ce22781d92316931ecbaf9f34b3d6
Sebastian Olsson <S.Olsson91@gmail.com>
2014-04-09 16:24:32 +0200

    Jobbat på Dörr scriptet och försökt få en push att fungera korrekt, ej klar

--- Assets/Scripts/Objects/Components/DoorDrag.cs
+++ Assets/Scripts/Objects/Components/DoorDrag.cs
@@ -6,7 +6,8 @@ using System.Collections;
  * 
  * Created by: Sebastian Olsson 2014-04-08
  * Modified by:
- * TODO: Check forward on doors
+ * TODO: Add so that the player can push the door
+ * TODO: Clean up script
  */
 
 public class DoorDrag : ObjectComponent
@@ -14,11 +15,13 @@ public class DoorDrag : ObjectComponent
 	#region PrivateMemberVariables
 	private float 		m_MouseXPosition;
 	private float 		m_MouseYPosition;
-	private float		m_DeActivateCounter	= 5;
-	private float		m_Speed				= 5.0f;
-	private bool		m_Closed 			= true;
-	private bool		m_UnlockedCamera	= true;
+	private float		m_DeActivateCounter		= 5;
+	private float		m_Speed					= 5.0f;
+	private bool		m_UnlockedCamera		= true;
 	private Transform   m_Camera;
+	private GameObject	m_Player;
+	private float 		m_Delta;
+	private Vector3		m_CurrentPlayerPosition;
 	#endregion
 	
 	#region PublicMemberVariables
@@ -26,11 +29,15 @@ public class DoorDrag : ObjectComponent
 	public string 		m_VerticalInput;
 	public string 		m_Input;
 	public GameObject	m_Target;
+	public bool			m_Closed			= true;
+	public string 		m_PlayerName		= "Player Controller Example";
 	#endregion
 	
 	void Start () 
 	{
-		m_Camera  = Camera.main.transform;
+		m_Camera = Camera.main.transform;
+		m_Player = GameObject.Find (m_PlayerName); 
+		m_CurrentPlayerPosition = m_Player.transform.position;
 	}
 	
 	void Update () 
@@ -51,25 +58,42 @@ public class DoorDrag : ObjectComponent
 		}
 	}
 
+	void OnCollisionStay()
+	{
+		float playerSpeed = m_Player.GetComponent<FirstPersonController>().m_MovementSpeed;
+		Debug.Log ("Hejsan Collision");
+
+		this.transform.parent.RotateAround (transform.parent.position, Vector3.up, -playerSpeed);
+	}
+
 	public override void Interact()
 	{
 		if(GetIsActive())
 		{
-			float m_Delta;
+			Vector3 m_RotationAxis;
 			m_MouseXPosition = Input.GetAxis(m_HorizontalInput);
 			m_MouseYPosition = Input.GetAxis(m_VerticalInput);
 
-			if(m_MouseXPosition > 0 || m_MouseYPosition > 0)
+			//Upplåst
+			if(m_Closed == true)
 			{
-				Debug.Log ("Större än");
-				m_Delta = (m_MouseYPosition + m_MouseXPosition) * m_Speed;
-				m_Target.transform.Rotate(0,-m_Delta, 0);
+				if(m_Target.transform.rotation.y != 0)
+				{
+					Debug.Log("Större än 0! = " + m_Target.transform.rotation.y);
+					m_Closed = false;
+
+				}
 			}
-			else if(m_MouseXPosition < 0 || m_MouseYPosition < 0)
+			else
 			{
-				Debug.Log ("Mindre än");
-				m_Delta = (m_MouseYPosition + m_MouseXPosition) * m_Speed;
-				m_Target.transform.Rotate(0,-m_Delta, 0);
+
+			}
+
+			if(m_MouseXPosition != 0 || m_MouseYPosition != 0)
+			{
+				m_RotationAxis = PlayerForward();
+				m_Target.transform.Rotate(m_RotationAxis,m_Delta);
+
 			}
 		}
 
@@ -85,4 +109,37 @@ public class DoorDrag : ObjectComponent
 			DeActivate();
 		}
 	}
+
+	//Works Okej, but far far far from perfect
+	private Vector3 PlayerForward()
+	{
+		Vector3 forward = new Vector3();
+
+		if(m_Player.transform.forward.z >= 0.7 && m_Player.transform.forward.x >= -0.7 && m_Player.transform.forward.x <= 0.7)
+		{
+			forward = new Vector3(0, 1 , 0);
+			m_Delta = (m_MouseYPosition + m_MouseXPosition) * m_Speed;
+			Debug.Log ("Forward 1 = " + forward);
+		}
+		else if(m_Player.transform.forward.z <= -0.7 && m_Player.transform.forward.x >= -0.7 && m_Player.transform.forward.x <= 0.7)
+		{
+			forward = new Vector3(0, -1, 0);
+			m_Delta = (m_MouseYPosition + -m_MouseXPosition) * m_Speed;
+			Debug.Log ("Forward 2 = " + forward);
+		}
+		else if(m_Player.transform.forward.x <= -0.7 && m_Player.transform.forward.z >= -0.7 && m_Player.transform.forward.z <=0.7)
+		{
+			forward = new Vector3(0, 1, 0);
+			m_Delta = (m_MouseYPosition + m_MouseXPosition) * m_Speed;
+			Debug.Log ("Forward 3 = " + forward);
+		}
+		else if(m_Player.transform.forward.x >= -0.7 && m_Player.transform.forward.z >= -0.7 && m_Player.transform.forward.z <=0.7)
+		{
+			forward = new Vector3(0, -1, 0);
+			m_Delta = (m_MouseYPosition + -m_MouseXPosition) * m_Speed;
+			Debug.Log ("Forward 4 = " + forward);
+		}
+		return forward;
+	}
+
 }
