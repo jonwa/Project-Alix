7b6d25e1f1ac5f0221582299511d594689c8dd67
Siiken <siik.cool@hotmail.com>
2014-04-22 16:26:49 +0200

    Changed movementlimit and pushable, and more rework with rotation limit, still not working

--- Assets/Scripts/Objects/Components/RotationLimit.cs
+++ Assets/Scripts/Objects/Components/RotationLimit.cs
@@ -19,27 +19,69 @@ public class RotationLimit : ObjectComponent
 	#endregion
 	
 	#region PrivateMemberVariables
-	private float m_RotationX = 0;
-	private float m_RotationY = 0;
-	private float m_RotationZ = 0;
-	private float m_LastRotationX;
-	private float m_LastRotationY;
-	private float m_LastRotationZ;
+	private Vector3 m_Rotation;
+	private Vector3 m_LastRotation;
+	private Vector3 m_Difference;
+	private Vector3 m_Offset;
+	private Vector3 m_OriginalRotation;
 	#endregion
 	
 	// Use this for initialization
 	void Start () 
 	{
+		m_OriginalRotation = transform.localRotation.eulerAngles;
+		m_LastRotation = m_OriginalRotation;
+		m_Offset = m_OriginalRotation - new Vector3(180,180,180);
+		Activate();
+		if(m_PositiveX > 180)
+		{
+			m_PositiveX = 180;
+		}
+		if(m_PositiveY > 180)
+		{
+			m_PositiveY = 180;
+		}
+		if(m_PositiveZ > 180)
+		{
+			m_PositiveZ = 180;
+		}
+		if(m_NegativeX > 180)
+		{
+			m_NegativeX = 180;
+		}
+		if(m_NegativeY > 180)
+		{
+			m_NegativeY = 180;
+		}
+		if(m_NegativeZ > 180)
+		{
+			m_NegativeZ = 180;
+		}
 		m_NegativeX *= -1;
 		m_NegativeY *= -1;
 		m_NegativeZ *= -1;
-		Activate();
+		m_Offset *= -1;
+		Debug.Log (m_Offset);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-
+		//if ((m_LastRotation.x + 5 > transform.localRotation.eulerAngles.x && m_LastRotation.x - 5 < transform.localRotation.eulerAngles.x)
+		//{
+		//	m_Difference = transform.localRotation.eulerAngles -(m_LastRotation);
+		//	Vector3 Rotate;
+		//	Rotate.x = CheckRotation (m_Difference.x, "x");
+		//	Rotate.y = CheckRotation (m_Difference.y, "y");
+		//	Rotate.z = CheckRotation (m_Difference.z, "z");
+		//	transform.Rotate (Rotate,Space.Self);
+		//	Debug.Log (m_Difference);
+		//	m_Difference *= 0;
+		//}
+		//m_LastRotation = transform.localRotation.eulerAngles;
+		//Debug.Log (m_LastRotation);
+		RotationHandler ();
+		m_LastRotation = transform.localRotation.eulerAngles;
 	}
 	
 	public float CheckRotation(float angle, string axis)
@@ -48,49 +90,100 @@ public class RotationLimit : ObjectComponent
 		axis.ToLower ();
 		if (axis.Equals ("x")) 
 		{
-			float CheckX = m_RotationX;
+			float CheckX = m_Rotation.x;
 			CheckX += angle;
-			Debug.Log(angle);
 			if(CheckX > m_PositiveX)
 			{
-				angle = m_PositiveX - m_RotationX; 
+				angle = m_PositiveX - m_Rotation.x; 
 			}
 			else if(CheckX < m_NegativeX)
 			{
-				angle = m_NegativeX + (m_RotationX * -1); 
+				angle = m_NegativeX + (m_Rotation.x * -1); 
 			}
-			m_RotationX +=angle;
+			m_Rotation.x +=angle;
 		}
 		else if (axis.Equals ("y")) 
 		{
-			float CheckY = m_RotationY;
+			float CheckY = m_Rotation.y;
 			CheckY += angle;
-			Debug.Log(angle);
 			if(CheckY > m_PositiveY)
 			{
-				angle = m_PositiveY - m_RotationY; 
+				angle = m_PositiveY - m_Rotation.y; 
 			}
 			else if(CheckY < m_NegativeY)
 			{
-				angle = m_NegativeY + (m_RotationY * -1); 
+				angle = m_NegativeY + (m_Rotation.y * -1); 
 			}
-			m_RotationY +=angle;
+			m_Rotation.y +=angle;
 		}
 		else if (axis.Equals ("z")) 
 		{
-			float CheckX = m_RotationX;
-			CheckX += angle;
-			Debug.Log(angle);
-			if(CheckX > m_PositiveX)
+			float CheckZ = m_Rotation.z;
+			CheckZ += angle;
+			if(CheckZ > m_PositiveX)
 			{
-				angle = m_PositiveX - m_RotationX; 
+				angle = m_PositiveX - m_Rotation.z; 
 			}
-			else if(CheckX < m_NegativeX)
+			else if(CheckZ < m_NegativeX)
 			{
-				angle = m_NegativeX + (m_RotationX * -1); 
+				angle = m_NegativeX + (m_Rotation.z * -1); 
 			}
-			m_RotationX +=angle;
+			m_Rotation.z +=angle;
 		}
 		return angle;
+	}
+	void RotationHandler()
+	{
+		float angle;
+		Vector3 current = transform.localRotation.eulerAngles + m_Offset;
+		Vector3 max;
+		max.x = m_OriginalRotation.x + m_Offset.x + m_PositiveX;
+		max.y = m_OriginalRotation.y + m_Offset.y + m_PositiveY;
+		max.z = m_OriginalRotation.z + m_Offset.z + m_PositiveZ;
+		Vector3 min;
+		min.x = m_OriginalRotation.x + m_Offset.x + m_NegativeX;
+		min.y = m_OriginalRotation.y + m_Offset.y + m_NegativeY;
+		min.z = m_OriginalRotation.z + m_Offset.z + m_NegativeZ;
+
+		if((current.x) > (max.x)){
+			angle = max.x - current.x;
+			angle = CheckRotation(angle, "x");
+			transform.Rotate(angle,0,0,Space.Self);
+		}
+		else if((current.x) < (min.x)){
+			angle = min.x + (current.x * -1);
+			angle = CheckRotation(angle, "x");
+			transform.Rotate(angle,0,0,Space.Self);
+		}
+		if((current.y) > (max.y)){
+			angle = max.y - current.y;
+			angle = CheckRotation(angle, "y");
+			transform.Rotate(0,angle,0,Space.Self);
+			Debug.Log(angle);
+		}
+		else if((current.y) < (min.y)){
+			angle = min.y + (current.y * -1);
+			angle = CheckRotation(angle, "y");
+			transform.Rotate(0,angle,0,Space.Self);
+		}
+		if((current.z) > (max.z)){
+			angle = max.z - current.z;
+			angle = CheckRotation(angle, "z");
+			transform.Rotate(0,0,angle,Space.Self);
+		}
+		else if((current.z) < (min.z)){
+			angle = min.z + (current.z * -1);
+			angle = CheckRotation(angle, "z");
+			transform.Rotate(0,0,angle,Space.Self);
+		}
+	}
+
+	void OnCollisionEnter()
+	{
+
+	}
+	void OnCollisionExit()
+	{
+
 	}
 }
