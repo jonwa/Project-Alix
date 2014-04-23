d6a0259d76519a742bab15e9a6bdf4c238bdf25a
Siiken <siik.cool@hotmail.com>
2014-04-23 11:34:47 +0200

    te

---
+++ Assets/Scripts/Objects/TriggerEffect.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+/* Discription: Base class for triggers on objects
+ * 
+ * Created by: 
+ * Modified by: 
+ * 
+ */
+
+public class TriggerEffect :  MonoBehaviour
+{
+	public virtual string Name
+	{ get{}}
+}
