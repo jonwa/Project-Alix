5fdbf799edc013791cd449a7dafa15098df5bc1c
Rasmus Björk <ramsus_89@hotmail.com>
2014-04-30 14:35:10 +0200

    hide and seek, and cleanup on portalscripts

---
+++ Assets/Rasmus/Scripts/HideScore.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideScore : MonoBehaviour 
+{
+	public GameObject one;
+	public GameObject two;
+	public GameObject three;
+	public float m_TimePerRound;
+
+	private int   m_Score;
+	private float m_GameTime;
+	private bool  m_Game = false;
+	// Use this for initialization
+	void Start () 
+	{
+		m_GameTime = m_TimePerRound;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(m_Game)
+		{
+			if(m_GameTime > 0)
+			{
+				m_GameTime -= Time.deltaTime;
+			}
+			else
+			{
+				Debug.Log("Game over! Total score: " + m_Score);
+				m_Score = 0;
+				m_GameTime = m_TimePerRound;
+				one.SendMessage("Restart");
+				two.SendMessage("Restart");
+				three.SendMessage("Restart");
+				m_Game = false;
+			}
+		}
+	}
+
+	public void AddScore()
+	{
+		m_Score++;
+	}
+
+	public void RestartGame()
+	{
+		if(m_Game == false)
+		{
+			m_Game = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Character/Camera/CameraFilter.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*Description: Script for adding an effect to the camera for fixed time
+
+Made by: Rasmus 30/04
+ */
+
+public class CameraFilter : MonoBehaviour 
+{
+	#region PublicMemberVariables
+	public Material[] m_Materials;
+	public float[]    m_Duration;
+	public int        m_WhatEffect;
+	#endregion
+
+	#region PrivateMemberVariables
+	private bool  m_EffectActive = false;
+	private float m_Timer   	 = 0;
+	#endregion
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown("o"))
+		{
+			UseEffect(m_WhatEffect);
+		}
+		if(m_EffectActive == true)
+		{
+			m_Timer -= Time.deltaTime;
+			if(m_Timer <= 0)
+			{
+				m_EffectActive = false;
+			}
+		}
+	}
+
+	public void UseEffect(int effect)
+	{
+		m_WhatEffect   = effect;
+		m_EffectActive = true;
+		m_Timer		   = m_Duration[m_WhatEffect];
+	}
+
+	//For future use with triggers
+	public void UseSelectedEffect()
+	{
+		UseEffect(m_WhatEffect);
+	}
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination)
+	{
+		if(m_EffectActive == true)
+		{
+			Graphics.Blit (source, destination, m_Materials[m_WhatEffect]);
+		}
+		else
+		{
+			Graphics.Blit (source, destination);
+		}
+	}
+}
