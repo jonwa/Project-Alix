dca32303fe4e4e5f065bd865eb6831b7244e12ca
Rasmus Björk <ramsus_89@hotmail.com>
2014-05-02 15:49:29 +0200

    oculus rift

--- Assets/Rasmus/Scripts/HideScore.cs
+++ Assets/Rasmus/Scripts/HideScore.cs
@@ -6,8 +6,11 @@ public class HideScore : MonoBehaviour
 	public GameObject one;
 	public GameObject two;
 	public GameObject three;
+	public GUIText m_text;
 	public float m_TimePerRound;
+	public GameObject m_player;
 
+	private Vector3 m_origPos;
 	private int   m_Score;
 	private float m_GameTime;
 	private bool  m_Game = false;
@@ -15,6 +18,8 @@ public class HideScore : MonoBehaviour
 	void Start () 
 	{
 		m_GameTime = m_TimePerRound;
+		m_origPos = m_player.transform.position;
+		m_text.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -28,15 +33,18 @@ public class HideScore : MonoBehaviour
 			}
 			else
 			{
-				Debug.Log("Game over! Total score: " + m_Score);
+				m_text.text = "Final score: " + m_Score.ToString();
+				m_text.gameObject.SetActive(true);
 				m_Score = 0;
+				m_player.transform.position = m_origPos;
 				m_GameTime = m_TimePerRound;
 				one.SendMessage("Restart");
 				two.SendMessage("Restart");
 				three.SendMessage("Restart");
 				m_Game = false;
+				Camera.main.GetComponent<CameraFilter>().UseSelectedEffect();
 			}
-		}
+		}	
 	}
 
 	public void AddScore()
@@ -46,6 +54,7 @@ public class HideScore : MonoBehaviour
 
 	public void RestartGame()
 	{
+		m_text.gameObject.SetActive(false);
 		if(m_Game == false)
 		{
 			m_Game = true;
