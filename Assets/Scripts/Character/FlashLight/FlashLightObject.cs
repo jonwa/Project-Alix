42ef04a801e16c491a4a3b51d7b0f7f5cb128fc1
Siiken <siik.cool@hotmail.com>
2014-05-08 15:43:49 +0200

    flashlight

---
+++ Assets/Scripts/Character/FlashLight/FlashLightObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashLightObject : ObjectComponent 
+{
+	public override void Interact()
+	{
+		GameObject.FindGameObjectWithTag("FlashLight").GetComponent<FlashLight>().Find();
+	}
+	public override void Serialize(ref JSONObject jsonObject){}
+	public override void Deserialize(ref JSONObject jsonObject){}
+}
