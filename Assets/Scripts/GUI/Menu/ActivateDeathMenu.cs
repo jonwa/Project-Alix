89b8c2113e1bc43ad8a3be818970477116a3e64f
jonwa <b12jonwa@SD30205.hs.local>
2014-05-12 16:22:18 +0200

    Death menu

---
+++ Assets/Scripts/GUI/Menu/ActivateDeathMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Used in order to open the deathmenu when the player dies. 
+ * The function Activate can be called for this. 
+ * 
+ * 
+ * Created By: Jon Wahlström 2014-05-12
+ * Modified By: 
+ */
+
+public class ActivateDeathMenu : MonoBehaviour 
+{
+	public GameObject _window; 
+
+	public void Activate() 
+	{
+		WindowStatus status = gameObject.GetComponent<WindowStatus>();
+		_window.SetActive(true);	
+		bool isActive = InputManager.RequestShowWindow(gameObject);
+		status.Activate(isActive);
+	}
+}
