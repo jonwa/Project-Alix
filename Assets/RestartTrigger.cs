cc104e2741db789d0986ae8be01b08059c4ea616
Martin Eriksson <haywer@gmail.com>
2014-05-14 09:33:09 +0200

    merge

---
+++ Assets/RestartTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		col.GetComponent<RestartWallJumpGrej> ().NewStartPoint (transform.position);
+	}
+}
