5b8f700eb51148fdcdc3dd3b0b7256f895c2fe4e
Martin Eriksson <haywer@gmail.com>
2014-05-19 09:37:57 +0200

    git merhe

---
+++ Assets/RestartScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartScene : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("m")) 
+		{
+			Application.LoadLevel("Surf1.0");
+		}
+	}
+}
