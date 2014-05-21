21b4b1caa8ab719f2f9d1e2b43feb08b3c281586
Jimmy Nilsson <Jiiiimmmyy@gmail.com>
2014-05-21 14:34:01 +0200

    merge robert

--- Assets/Scenes/Jimmy/GravityChangeField.cs
+++ Assets/Scenes/Jimmy/GravityChangeField.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class GravityChangeField : MonoBehaviour 
 {
+public enum direction { up, down, left, right }
+	public direction _Direction = direction.up;
 	int counter = 0;
 	bool shouldTurn = false;
 	Transform player = null;
+	
+	
 	
 	void OnTriggerEnter(Collider other)
 	{
@@ -14,7 +18,21 @@ public class GravityChangeField : MonoBehaviour
 			shouldTurn = true;
 			player = other.transform;
 			other.gameObject.AddComponent<ConstantForce>();
-			other.gameObject.constantForce.force = new Vector3(0,2f,0);
+			
+			if(_Direction == direction.up)
+			{
+				other.gameObject.constantForce.force = new Vector3(0,2f,0);
+			}
+			else if(_Direction == direction.left)
+			{
+				other.gameObject.constantForce.force = new Vector3(-2,0f,0);
+				other.rigidbody.useGravity = false;
+			}
+			else if(_Direction == direction.right)
+			{
+				other.gameObject.constantForce.force = new Vector3(2,0f,0);
+				other.rigidbody.useGravity = false;
+			}
 		}
 	}
 	
@@ -22,13 +40,39 @@ public class GravityChangeField : MonoBehaviour
 	{
 		if(shouldTurn)
 		{
-			counter++;
-			player.RotateAround(player.position, Vector3.forward, 2);
-			if(counter >= 90)
+		
+			if(_Direction == direction.up)
 			{
-				shouldTurn = false;
-				counter = 0;
+				counter++;
+				player.RotateAround(player.position, Vector3.forward, 2);
+				if(counter >= 90)
+				{
+					shouldTurn = false;
+					counter = 0;
+				}
 			}
+			else if(_Direction == direction.left)
+			{
+				counter++;
+				player.RotateAround(player.position, Vector3.forward, 2);
+				if(counter >= 45)
+				{
+					shouldTurn = false;
+					counter = 0;
+				}
+			}
+			else if(_Direction == direction.right)
+			{
+				counter++;
+				player.RotateAround(player.position, Vector3.forward, -2);
+				if(counter >= 45)
+				{
+					shouldTurn = false;
+					counter = 0;
+				}			
+			}
+		
+
 		}
 	}
 	
@@ -36,6 +80,7 @@ public class GravityChangeField : MonoBehaviour
 	{
 		if(other.tag == "Player")
 		{
+			other.rigidbody.useGravity = true;
 			shouldTurn = true;
 			player = other.transform;
 			Destroy(other.gameObject.GetComponent<ConstantForce>());
