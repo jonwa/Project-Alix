b3820a70c316f2ef15a8068c69d3ba2c96d3b399
Martin Eriksson <haywer@gmail.com>
2014-05-28 14:57:12 +0200

    working menues and stuff // jon

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -3,25 +3,32 @@ using System.Collections;
 
 public class LoadingScreen : MonoBehaviour {
 
+	public string m_LevelToLoad = "Main5";
+	public float m_TimeUntilNextLoad = 20f;
+
 	AsyncOperation async;
 	float loading = 0;
-	GameObject trans;
+
+
+
+	//GameObject trans;
 
 	// Use this for initialization
 	void Start() {
 		StartLoading();
-		trans = GameObject.FindWithTag ("Progressbar");
+		//trans = GameObject.FindWithTag ("Progressbar");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		m_TimeUntilNextLoad -= Time.deltaTime;
 		float progress = async.progress;
-		if(Input.GetButtonDown("Fire2"))
+		if(Input.GetButtonDown("Fire2") || m_TimeUntilNextLoad <= 0f)
 		{
 			ActivateScene();
 		}
 		Debug.Log (progress);
-		trans.transform.localScale = new Vector3(progress*11,1,0);
+		//trans.transform.localScale = new Vector3(progress*11,1,0);
 		loading++;
 	}
 	
@@ -30,7 +37,7 @@ public class LoadingScreen : MonoBehaviour {
 	}
 	
 	IEnumerator load() {
-		async = Application.LoadLevelAsync("Robert");
+		async = Application.LoadLevelAsync(m_LevelToLoad);
 		async.allowSceneActivation = false;
 		yield return async;
 	}
